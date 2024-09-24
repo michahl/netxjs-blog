f3c5567ee6abbbd2dac5fbd787d1fd433b47a5fc
Michail Christoforatos <mchristoforatos@icloud.com>
2024-09-25 01:18:21 +0300

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Header from './components/Header'
 
 const App = () => {
   return (
-    <div className='w-full flex flex-col justify-center items-center h-screen'>
+    <div className='w-full flex flex-col justify-center items-center min-h-screen'>
       <div className='w-full max-w-2xl'>
         <div className='mx-10 lg:mx-0'>
           <div className='flex flex-col justify-around'>
             <Header />
 
-
+            <p>main section</p>
             <Footer />
           </div>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { LuSunMedium } from "react-icons/lu";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className="fixed top-0 left-0 w-full z-50 pt-10 pb-5 backdrop-blur-sm">
+        <div className="max-w-2xl mx-auto">
+            <div className="flex justify-end">
+                <div className="flex items-center gap-5">
+                    <div className="flex flex-row gap-8 text-gray-600">
+                        <a href="/" className="text-sm hover:text-gray-800">Home</a>
+                        <a href="/blog" className="text-sm hover:text-gray-800">Blog</a>
+                        <a href="/tags" className="text-sm hover:text-gray-800">Tags</a>
+                        <a href="/about" className="text-sm hover:text-gray-800">About</a>
+                    </div>
+                    <div className="md:h-5 md:w-px md:bg-zinc-900/10"></div>
+                    <button className="h-6 w-6 flex items-center justify-center rounded-sm hover:text-gray-800" aria-label="switch theme mode">
+                        <LuSunMedium />
+                    </button>
+                </div>
+            </div>
+        </div>
+    </nav>
   )
 }
 
