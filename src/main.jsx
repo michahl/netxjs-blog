b965c3b04ac96ae6aabc3bd21ed68a8616f4ff81
Michail Christoforatos <mchristoforatos@icloud.com>
2024-09-25 12:25:48 +0300

    added about page

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import Blog from './pages/Blog.jsx'
 import Tags from './pages/Tags.jsx'
-import './index.css'
+import About from './pages/About.jsx'
 
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route  path='/' element={<App />}/>
-        <Route  path='/blog' element={<Blog />}/>
-        <Route  path='/tags' element={<Tags />}/>
+        <Route path='/' element={<App />}/>
+        <Route path='/blog' element={<Blog />}/>
+        <Route path='/tags' element={<Tags />}/>
+        <Route path='/about' element={<About />}/>
       </Routes>
     </Router>
   </StrictMode>,
