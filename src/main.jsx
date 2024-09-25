609d1fb3a74deee55302b5fcb577af7121ed27d6
Michail Christoforatos <mchristoforatos@icloud.com>
2024-09-25 12:24:10 +0300

    added blog & tags pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import App from './App.jsx'
+import Blog from './pages/Blog.jsx'
+import Tags from './pages/Tags.jsx'
 import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-      <App />
+      <Routes>
+        <Route  path='/' element={<App />}/>
+        <Route  path='/blog' element={<Blog />}/>
+        <Route  path='/tags' element={<Tags />}/>
+      </Routes>
     </Router>
   </StrictMode>,
 )
