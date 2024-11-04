7352159d496989dc3efbf0188ab4a476099f56cc
Michail Christoforatos <mchristoforatos@icloud.com>
2024-11-04 15:00:01 +0200

    add function to retrieve all tags from posts

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -27,3 +27,24 @@ export function getAllPosts() {
 
   return posts;
 }
+// Function to get all tags from posts
+export function getAllTags() {
+  const files = fs.readdirSync(POSTS_FOLDER); // Read all files in the 'posts' directory
+
+  const tags = new Set();
+
+  files.forEach((filename) => {
+    // Read the file contents
+    const filePath = path.join(POSTS_FOLDER, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+
+    // Use gray-matter to parse the file frontmatter
+    const { data: frontmatter } = matter(fileContents);
+
+    if (frontmatter.tags) {
+      frontmatter.tags.forEach((tag) => tags.add(tag));
+    }
+  });
+
+  return Array.from(tags);
+}
