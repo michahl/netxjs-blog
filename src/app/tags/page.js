b0afdc24f734a916c8911cc669f97c7c65eb739f
Michail Christoforatos <mchristoforatos@icloud.com>
2024-11-04 20:24:05 +0200

    add tags page to display topics with links and badges

---
+++ src/app/tags/page.js
@@ -0,0 +1,34 @@
+import Header from "@/components/header";
+import { Badge } from "@/components/ui/badge";
+import { getAllTags } from "@/lib/posts";
+import Link from "next/link";
+
+export default function Tags() {
+    const tags = getAllTags();
+    return (
+        <div className="flex justify-center min-h-screen">
+            <div className="max-w-2xl w-full mx-2 sm:mx-5 relative">
+                <div className="my-12">
+                    <Header />
+        
+                    <div className="mt-10">
+                        <h3 className="text-lg font-semibold">Topics I've written about</h3>
+                    </div>
+                    <div className="flex flex-wrap gap-2 mt-5">
+                    {
+                        tags.map(tag => (
+                            <Link 
+                                key={tag} 
+                                className="flex justify-between items-center"
+                                href={`/tags/${tag}`}
+                            >
+                                <Badge variant="outline" className="hover:bg-zinc-100/50 dark:hover:bg-zinc-800/50 rounded-2xl text-sm">{tag}</Badge>
+                            </Link>
+                        ))
+                    }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
